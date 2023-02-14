bb362247fc44424a172b459c64403ab3c9a88191
prateekvarma <prtk.varma@gmail.com>
2023-02-14 15:19:43 -0600

    Read csv data using createReadStream using fs module

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-const { parse } = require('parse');
-const { fs } = require('fs');
+const { parse } = require('csv-parse');
+const fs = require('fs');
+
+const results = [];
+
+fs.createReadStream('kepler-data.csv')
+  .on('data', (data) => {
+    results.push(data);
+  })
+  .on('error', (err) => {
+    console.log('There has been an error: ', err);
+  })
+  .on('end', () => {
+    console.log('Final result: ', results);
+    console.log('done.');
+  });

c6687f92758eede66b9da6e64927bd415a3fc7b5
prateekvarma <prtk.varma@gmail.com>
2023-02-15 15:42:07 -0600

    Added checks to see favorable conditions that determine habitable environment

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const { parse } = require('csv-parse');
 const fs = require('fs');
 
-const results = [];
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] > 1.11
+    && planet['koi_prad'] < 1.6; // for a planet to be habitable, the koi_insol shold be between 0.36 & 1.11 & radius ratio shold be less than 1.6
+}
 
 fs.createReadStream('kepler-data.csv')
-  .pipe(parse({
-    comment: '#',
-    columns: true
-  }))
+  .pipe(
+    parse({
+      comment: '#',
+      columns: true,
+    })
+  )
   .on('data', (data) => {
-    results.push(data);
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
   })
   .on('error', (err) => {
     console.log('There has been an error: ', err);
   })
   .on('end', () => {
-    console.log('Final result: ', results);
+    console.log(`${habitablePlanets.length} habitable planets found!`);
     console.log('done.');
   });
